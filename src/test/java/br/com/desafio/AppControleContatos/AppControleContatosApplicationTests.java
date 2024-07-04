970a54ace1c1dac2c15e7296071a9a99f713f4a8
JuanLucca846 <lucca470@gmail.com>
2024-07-04 13:55:39 -0300

    initial commit

---
+++ src/test/java/br/com/desafio/AppControleContatos/AppControleContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.AppControleContatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppControleContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
