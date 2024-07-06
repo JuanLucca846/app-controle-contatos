6d57b7d606857c16872eac9fecaa601e5a87f6ad
JuanLucca846 <lucca470@gmail.com>
2024-07-06 00:51:27 -0300

    docs: Documentação com Swagger

---
+++ src/main/java/br/com/desafio/AppControleContatos/configuration/OpenAPIConfig.java
@@ -0,0 +1,35 @@
+package br.com.desafio.AppControleContatos.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(
+                        new Components().addSecuritySchemes("basicScheme",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                                        .scheme("basic")
+                        )
+                )
+                .info(
+                        new Info()
+                                .title("App de controle de contatos")
+                                .description("Faz o controle de contatos das pessoas")
+                                .contact(new Contact()
+                                        .name("Juan Lucca Santana dos Santos")
+                                        .email("lucca470@gmail.com")
+                                        .url("http://localhost:8080/swagger-ui.html")
+                                )
+                                .version("Versão 1.0")
+                );
+    }
+}
